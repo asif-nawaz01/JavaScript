6a7764c66ddca28947106f7fb578e0cdf0066741
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-06-01 22:03:18 +0530

    String

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -95,6 +95,8 @@ const first_Name = "Asif";
 const last_Name = "Nawaz";
 console.log(first_Name + " " + last_Name);
 
+// *******Assignment Operators******
+
 let x = 10 + 5; // 15
 x += 10; // x = x + 10 = 25
 x *= 4; // x = x * 4 = 100
@@ -103,9 +105,67 @@ x--;
 x--;
 console.log(x);
 
+// *******Comparision Assignment*****
+
 console.log(ageVikky > ageRaj); // >,<,>=,<=
 console.log(ageRaj >= 18);
 
 const isFullAge = ageRaj >= 18;
 
 console.log(now - 1991 > now - 2019);
+
+let a , b;
+a=y=25-10-5; // a=10, b=10;
+console.log(a,y);
+
+const averageAge = (ageVikky + ageRaj)/2;
+console.log(ageVikky, ageRaj, averageAge);
+
+// const description = 'Indians is in India, and its 143 Million people speak Hindi';
+
+// ******Challenges #1********
+
+//*****Question_01******
+const massMark = 78;
+const heightMark =1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+const BMIMark = massMark / heightMark **2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark,BMIJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
+//*******Question_02********
+
+const marksWeight2 = 95;
+const marksHeight2 = 1.88;
+
+const johnWeight2 = 85;
+const JohnHeight2 = 1.76;
+
+let markBMI2 = marksWeight2/(marksHeight2 ** 2);
+let johnBMI2 = johnWeight2/(JohnHeight2 ** 2);
+
+let markHigherBMI2 = markBMI2 > johnBMI2;
+console.log(markBMI2, johnBMI2, markHigherBMI2);
+
+//Strings 
+const Name = "Nawaz";
+const Job = "Programmer";
+const birtYear = 2003;
+const Year = 2025;
+
+const jonas = "I am " + firstName + ", a " + (Year - birtYear) + " years old person, with a profession of " + Job;
+console.log(jonas);
+
+const jonasNew = `I am ${firstName}, a ${Year - birtYear} years old ${Job}`;
+console.log(jonasNew);
+
+console.log(`Just a normal String......`);
+
+console.log('String with \n multiple\n line');
+console.log(`This is a
+multiple line 
+comment`);
