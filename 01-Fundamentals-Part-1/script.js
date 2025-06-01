987003da4512d5f68c200df23c4efa6cd3cd6742
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-06-01 19:34:36 +0530

    Variables in JS

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,39 @@
  let js ='amazing';
-    if (js==="amazing") alert("JavaScript is FUN");
+    if (js==="amazing") 
     console.log(40+8+13-10);
-
     
+console.log("joanas");
+console.log('143');
+    
+//****** values & variables******
+
+let firstName = "JavaScript";
+let first = 'jonas';
+
+console.log(firstName);
+console.log(first);
+console.log(firstName);
+
+let Jonas_Matilda = "JM";
+let _new = 77;
+let name = "Jonas";
+
+// Naming Conventions
+
+let myFisrtJob = "programmer";
+let myCurrentJob = "Teacher";
+
+let Job1 = 'programmer';
+let Job2 = 'Teacher';
+
+console.log(myCurrentJob);
+
+let Country = "India";
+let Continent = "Asia";
+let population = "4.500 M";
+
+console.log("Continent is " + Continent);
+console.log("Country is " + Country);
+console.log("The population is " + population);
+
+
