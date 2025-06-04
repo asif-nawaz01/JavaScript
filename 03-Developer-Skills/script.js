378038ac8cbbc2118c60f215df17139ed068e7d7
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-06-04 21:36:47 +0530

    Challenges

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -32,3 +32,46 @@ const calcTempAmplitude = function (temps) {
 const amplitude = calcTempAmplitude(temperatures);
 console.log(amplitude);
 
+//_________PROBLEM 2_________
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
+
+
+
+//---------CODING CHALLENGE #1-----------
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+console.log(`... ${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ...`);
+
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC in ${i + 1} days ... `;
+  }
+  console.log('...' + str);
+};
+printForecast(data1);
+
+
+
+////////////////--------------- COMPLETED ------------------//////////////////
