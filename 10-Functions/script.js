ed66ef57d8f91e0402665efa9da974c6cdb35481
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-06-05 20:08:30 +0530

    Function

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -71,3 +71,83 @@ checkIn(flight.Adity);
 const oneword = function(str) {
     return str.replace( /  /g, '').toLowerCase();
 };
+
+// FUNCTION ACCEPTING CALLBACK FUNCTIONS
+
+const oneWord = function(str) {
+    return str.replace(/ /g, ' ').toLowerCase();
+}
+
+const upperFirstWord = function(str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+}
+
+//Higher-order function
+const transformer = function(str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+
+    console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS callbacks all the time
+
+const high5 = function() {
+    console.log('high5');
+};
+document.body.addEventListener('click', high5);
+
+['Riyanshu', 'Saloni', 'Aisha', 'Supriya'].forEach(high5);
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------//
+
+// FUNCTION RETURNING FUNCTIONS
+
+const greet = function(greeting) {
+    return function(name) {
+        console.log(`${greeting} ${name}`);
+    };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Nawaz');
+greeterHey('Shrii');
+
+greet('Hello')('NawaZ');
+
+// CHALLENGE
+const greetArr = greeting => Name => console.log(`${greeting} ${Name}`);
+
+greetArr('Hi')('Nawaz');
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------//
+
+// THE CALL AND APPLY METHODS
+
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    // book: function() {}
+    book(flightNum, name) {
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+
+        this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name});
+    },
+};
+
+lufthansa.book(239, 'Shrii Mishra');
+lufthansa.book(239, 'Sakshi Shrii');
+console.log(lufthansa);
+
+const eurowings = {
+    airline: 'Eurowings',
+    iataCode: 'EW',
+    bookings: [],
+};
