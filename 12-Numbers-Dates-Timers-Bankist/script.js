24818e9a81c6d426a1e461e26268400524d7479d
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-06-05 23:42:28 +0530

    Numberss

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -251,3 +251,95 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+
+// CONVERTING AND CHECKING NUMBERS
+
+console.log(23 == 23.0);
+
+// Base 10 - 0 to 9
+// Binary Base 2 - 0 1
+
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 == 0.3);
+
+// Conversion
+
+console.log(Number(23));
+console.log(+'23');
+
+// Parsing
+
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('e23', 10));
+
+console.log(Number.parseInt(' 2.5rem '));
+console.log(Number.parseFloat(' 2.5rem '));
+
+//console.log(parseFloat('2.5rem'));
+
+// Check if value is NaN
+
+console.log(Number.isNaN(20));
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(+'20X'));
+console.log(Number.isNaN(23 / 0));
+
+// Checking if value is number
+
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('20'));
+console.log(Number.isFinite(+'20'));
+console.log(Number.isFinite(23 / 0));
+
+console.log(Number.isInteger(23));
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger(23 / 0));
+
+
+
+
+// MATH AND ROUNDING
+
+console.log(Math.sqrt(25));
+console.log(25 ** (1/2));
+console.log(8 ** (1/3));
+
+console.log(Math.max(5, 18, 10, 25, 56));
+console.log(Math.max(5, 18, 10, 25, '56'));
+console.log(Math.max(5, 18, 10, 25, '56px'));
+
+console.log(Math.min(29, 88, 100, 25, 56));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) => Math.floor(Math.random () * (max - min) + 1) + min;
+
+// 0...1 -> 0...(max - min) -> min...max
+
+console.log(randomInt(10, 20));
+console.log(randomInt(0, 3));
+
+// Rounding integers
+
+console.log(Math.round(23.3));  
+console.log(Math.round(23.9));  
+
+console.log(Math.ceil(23.3)); 
+console.log(Math.ceil(23.9)); 
+
+console.log(Math.floor(23.3));  
+console.log(Math.floor('23.9')); 
+
+console.log(Math.trunc(23.3));  
+
+console.log(Math.trunc(-23.3));  
+console.log(Math.floor(-23.3));   
+
+// Rounding decimals
+
+console.log((2.7).toFixed(0));  
+console.log((2.7).toFixed(3));  
+console.log((2.345).toFixed(2));  
+console.log(+(2.345).toFixed(2));  
