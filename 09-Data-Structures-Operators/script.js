9ac180557b99b6571ee0efcdbbd8e6cfa7a2681e
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-06-05 09:18:17 +0530

    user

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -459,36 +459,66 @@
 
 // --------------OPTIONAL CHAINING-------------------
 
-if (restaurant.openingHours && restaurant.openingHours.mon)
-  console.log(restaurant.openingHours.mon.open);
+// if (restaurant.openingHours && restaurant.openingHours.mon)
+//   console.log(restaurant.openingHours.mon.open);
 
-// console.log(restaurant.openingHours.mon.open);
+// // console.log(restaurant.openingHours.mon.open);
 
-// ___________WITH optional chaining____________
+// // ___________WITH optional chaining____________
 
-console.log(restaurant.openingHours.mon?.open);
-console.log(restaurant.openingHours?.mon?.open);vd 
+// console.log(restaurant.openingHours.mon?.open);
+// console.log(restaurant.openingHours?.mon?.open);vd 
 
-// ^^^^^^^^^^^^^^Example^^^^^^^^^^^^^^^^^^
+// // ^^^^^^^^^^^^^^Example^^^^^^^^^^^^^^^^^^
 
-const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
-for (const day of days) {
-  const open = restaurant.openingHours[day]?.open ?? 'closed';
-  console.log(`On ${day}, we open at ${open}`);
-}
+// for (const day of days) {
+//   const open = restaurant.openingHours[day]?.open ?? 'closed';
+//   console.log(`On ${day}, we open at ${open}`);
+// }
 
-// Methods^
+// // Methods^
 
-console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
-console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+// console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
 
-// Arrays
-const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+// // Arrays
 
-// const users = [];
+// const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
 
-console.log(users[0]?.name ?? 'User array empty');
+// // const users = [];
 
-if (users.length > 0) console.log(users[0].name);
-else console.log('user array empty');
+// console.log(users[0]?.name ?? 'User array empty');
+
+// if (users.length > 0) console.log(users[0].name);
+// else console.log('user array empty');
+
+// LOOPING OBJECTS: Object Keys, Values, and Entries
+
+// Property NAMES
+
+// const properties = Object.keys(openingHours);
+// console.log(properties);
+
+// let openStr = `We are open on ${properties.length} days: `;
+// for (const day of properties) {
+//   openStr += `${day}, `;
+// }
+// console.log(openStr);
+
+// // Property VALUES
+
+// const values = Object.values(openingHours);
+// console.log(values);
+
+// // Entire object
+
+// const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// // [key, value]
+
+// for (const [day, { open, close }] of entries) {
+//   console.log(`On ${day} we open at ${open} and close at ${close}`);
+// }
