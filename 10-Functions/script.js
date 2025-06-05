41042bc8fb82947f2fab927a2a87030343ed73ba
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-06-05 20:03:27 +0530

    Script10

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -30,3 +30,44 @@ createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
 
 
+// HOW PASSING ARGUMENTS WORKS: VALUE(Yes) vs REFERENCE(No)
+
+const flight = 'LH234';
+const Anshu = {
+    name: 'Shrii Mishra',
+    passport: 1234567890,
+}
+
+const checkIn = function(flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mrs.' + passenger.name;
+
+    if(passenger.passport === 1234567890) {
+        alert('Checked in');
+    }else{
+        alert('Wrong passport');
+    }
+};
+
+
+checkIn(flight, Adity);
+console.log(flight);
+console.log(adii);
+
+//Is the same as doing
+
+const flightNum = flight ;
+const passenger = Adity;
+
+
+
+const newPassport = function(person) {
+    person.passport = Math.trunc(Math.random()* 1000000000);
+}; 
+
+newPassport(Adity);
+checkIn(flight.Adity);
+
+const oneword = function(str) {
+    return str.replace( /  /g, '').toLowerCase();
+};
