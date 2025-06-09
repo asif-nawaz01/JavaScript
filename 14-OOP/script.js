85763b20fc1fe3a275ad4a881af43cb2f15ff6f1
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-06-09 20:03:04 +0530

    OOPS

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -185,29 +185,62 @@
 
 
 // Object.create
-const PersonProto = {
-  calcAge() {
-    console.log(2037 - this.birthYear);
-  },
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   },
 
-  init(firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
-  },
-};
+//   init(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   },
+// };
 
-const Sakshi = Object.create(PersonProto);
-console.log(Sakshi);
-Sakshi.name = 'Sakshi';
-Sakshi.birthYear = 2004;
-Sakshi.calcAge();
+// const Sakshi = Object.create(PersonProto);
+// console.log(Sakshi);
+// Sakshi.name = 'Sakshi';
+// Sakshi.birthYear = 2004;
+// Sakshi.calcAge();
 
-console.log(Sakshi.__proto__ === PersonProto);
+// console.log(Sakshi.__proto__ === PersonProto);
 
-const Sweta = Object.create(PersonProto);
-Sweta.init('Sweta', 2005);
-Sweta.calcAge();
+// const Sweta = Object.create(PersonProto);
+// Sweta.init('Sweta', 2005);
+// Sweta.calcAge();
 
-const Shriii = Object.create(PersonProto);
-Shriii.init('Shriii', 2000);
-Shriii.calcAge();
+// const Shriii = Object.create(PersonProto);
+// Shriii.init('Shriii', 2000);
+// Shriii.calcAge();
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() { 
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new CarCl('Ford', 120);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50;
+console.log(ford); 
