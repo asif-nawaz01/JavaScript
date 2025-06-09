d5e6a07efd65f9045840c8ecc7a9dfb1e94998ae
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-06-09 19:55:05 +0530

    OOPS

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -29,26 +29,76 @@
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // PROTOTYPES
-console.log(Person.prototype);
+// console.log(Person.prototype);
 
-Person.prototype.calcAge = function () {
-  console.log(2025 - this.birthYear);
-};
+// Person.prototype.calcAge = function () {
+//   console.log(2025 - this.birthYear);
+// };
 
-Aashu.calcAge();
-Nawaz.calcAge();
+// Aashu.calcAge();
+// Nawaz.calcAge();
 
-console.log(Aashu.__proto__);
-console.log(Nawaz.__proto__ === Person.prototype);
+// console.log(Aashu.__proto__);
+// console.log(Nawaz.__proto__ === Person.prototype);
 
-console.log(Person.prototype.isPrototypeOf(Aashu));
-console.log(Person.prototype.isPrototypeOf(Riyanshu));
-console.log(Person.prototype.isPrototypeOf(Person));
+// console.log(Person.prototype.isPrototypeOf(Aashu));
+// console.log(Person.prototype.isPrototypeOf(Riyanshu));
+// console.log(Person.prototype.isPrototypeOf(Person));
 
-// .prototyeOfLinkedObjects
+// // .prototyeOfLinkedObjects
 
-Person.prototype.species = 'XOXO Sapiens';
-console.log(Aashu.species, Nawaz.species);
+// Person.prototype.species = 'XOXO Sapiens';
+// console.log(Aashu.species, Nawaz.species);
 
-console.log(Aashu.hasOwnProperty('firstName'));
-console.log(Nawaz.hasOwnProperty('species'));
+// console.log(Aashu.hasOwnProperty('firstName'));
+// console.log(Nawaz.hasOwnProperty('species'));
+
+// PROTOTYPAL INHERITANCE ON BUILT-IN OBJECTS
+
+// console.log(Aashu.__proto__);
+
+// Object.prototype (top of prototype chain)
+// console.log(Aashu.__proto__.__proto__);
+// console.log(Aashu.__proto__.__proto__.__proto__);
+
+// console.dir(Person.prototype.constructor);
+
+// const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
+
+// console.log(arr.__proto__.__proto__);
+
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
+
+// const h1 = document.querySelector('h1');
+// console.dir(x => x + 1);
+
+
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(`${this.make} is going at ${this.speed} km/h`);
+// };
+
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(`${this.make} is going at ${this.speed} km/h`);
+// };
+
+// const bmw = new Car('BMW', 120);
+// const mercedes = new Car('Mercedes', 95);
+
+// bmw.accelerate();
+// bmw.accelerate();
+// bmw.brake();
+// bmw.accelerate();
+
