d319d09c01e26c0907edf64862eb267620632cfd
asif-nawaz01 <asifnawaz51111@gmail.com>
2025-06-09 19:48:31 +0530

    oops

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// CONSTRUCTOR FUNCTION AND NEW OPERATOR
+const Person = function(firstName, birthYear) {
+    // Instances properties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    // Never to this
+    //this.calcAge = function(){
+    //   console.log(2025 - birthYear);
+    // };
+};
+
+const Aashu = new Person('Aashu', 2003);
+console.log(Anshu);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const Nawaz = new Person('Nawaz', 2005);
+const Shrii = new Person('Shrii', 2005);
+console.log(Nawaz, Shrii);
+
+console.log(Aashu instanceof Person);
